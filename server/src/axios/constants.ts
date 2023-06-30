8c7954029e0dad62121966ec79dc1593f22f8033
apmfree78 <amit@profitswami.com>
2023-06-30 14:21:06 -0400

    fix issue with undefined env variable

--- server/src/axios/constants.ts
+++ server/src/axios/constants.ts
@@ -1,3 +1,9 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({
+  path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV}`),
+});
+
 export const databaseAPI = `${process.env.POCKETBASE_URL}/api/`;
 
 export const queryKeys = {
